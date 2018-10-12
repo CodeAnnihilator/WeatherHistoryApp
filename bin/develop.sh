46291c7648c9a4f9208da5f73d0c2956b9c8423c
Eugene <frostberryart@gmail.com>
2018-10-12 17:24:36 +0300

    added .gitignore, fixed types and warnings

--- bin/develop.sh
+++ bin/develop.sh
@@ -6,7 +6,7 @@ source bin/env.sh
 
 dcdev build
 echo "installing frontend deps"
-./bin/npm_frontend.sh i -q
+./bin/npm_frontend.sh i -q --no-optional
 echo "installing backend deps"
-./bin/npm_backend.sh i -q
+./bin/npm_backend.sh i -q --no-optional
 dcdev up
